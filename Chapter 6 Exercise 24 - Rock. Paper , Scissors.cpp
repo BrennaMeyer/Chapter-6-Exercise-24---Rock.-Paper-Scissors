eca11bcdf74d835190a94e7350f88458c3299f96
Brenna Meyer <brennameyer9@gmail.com>
2025-03-30 19:06:54 -0400

    done

--- Chapter 6 Exercise 24 - Rock. Paper , Scissors.cpp
+++ Chapter 6 Exercise 24 - Rock. Paper , Scissors.cpp
@@ -8,10 +8,99 @@
 //The computer’s choice is displayed.
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-int main()
+using namespace std;
+
+//Varables 
+const int rock = 1,
+paper = 2,
+scissors = 3;
+
+//Compter Choice
+int ComputerChoice()
 {
-  
+    return (rand() % 3) + 1;
 }
 
+int getValidChoice()
+{
+    int choice;
+    while (true) {
+        cout << "Choose (1) Rock, (2) Paper, or (3) Scissors: ";
 
+        if (cin >> choice && choice >= 1 && choice <= 3) {
+            cin.ignore(100, '\n');
+            return choice;
+        }
+
+        cout << "Invalid input. Please enter 1, 2, or 3.\n";
+        cin.clear(); // Clear error flags
+        cin.ignore(100, '\n');
+    }
+}
+
+//Choice and winner 
+void displayChoice(int choice)
+{
+    switch (choice) {
+    case rock:  cout << "Rock"; break;
+    case paper:  cout << "Paper"; break;
+    case  scissors: cout << "Scissors"; break;
+    }
+}
+
+//Winner 
+void determineWinner(int computer, int user, bool& playAgain)
+{
+    if (computer == user) {
+        cout << " Tie. Play again to determine the winner.\n";
+        playAgain = true;
+        return;
+    }
+
+    bool userWins =
+        (user == rock && computer == scissors) ||
+        (user == paper && computer == rock) ||
+        (user == scissors && computer == paper);
+
+    cout << "\n" << (userWins ? "You win" : "Computer wins") << ": ";
+
+    if (user == rock && computer == scissors)
+        cout << "Rock smashes scissors!";
+    else if (user == paper && computer == rock)
+        cout << "Paper covers rock!";
+    else if (user == scissors && computer == paper)
+        cout << "Scissors cut paper!";
+    else if (computer == rock && user == scissors)
+        cout << "Rock smashes scissors!";
+    else if (computer == paper && user == rock)
+        cout << "Paper covers rock!";
+    else if (computer == scissors && user == paper)
+        cout << "Scissors cut paper!";
+
+    cout << "\n";
+    playAgain = false;
+}
+
+//Main
+int main()
+{
+    srand(time(0)); 
+
+    bool playAgain;
+
+    do {
+        int computer = ComputerChoice();
+        int user = getValidChoice();
+
+        cout << "Computer chose: ";
+        displayChoice(computer);
+
+        determineWinner(computer, user, playAgain);
+
+    } while (playAgain);
+
+    return 0;
+}
