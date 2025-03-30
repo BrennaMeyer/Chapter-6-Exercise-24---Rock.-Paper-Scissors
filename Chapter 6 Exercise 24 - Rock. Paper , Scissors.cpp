f46df1b433e80c37a0777b5bb2e8d7d2022c705a
Brenna Meyer <brennameyer9@gmail.com>
2025-03-30 15:57:06 -0400

    Add project files.

---
+++ Chapter 6 Exercise 24 - Rock. Paper , Scissors.cpp
@@ -0,0 +1,17 @@
+// Chapter 6 Exercise 24 - Rock. Paper , Scissors.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Programmer: Brenna Meyer 
+// Date: March 30, 2025 
+// Requirements: When the program begins, a random number in the range of 1 through 3 is generated. 
+// If the number is 1, then the computer has chosen rock. If the number is 2, then the computer has chosen paper.
+// If the number is 3, then the computer has chosen scissors. (Don’t display the computer’s choice yet.)
+//The user enters his or her choice of “rock”, “paper”, or “scissors” at the keyboard. (You can use a menu if you prefer.)
+//The computer’s choice is displayed.
+
+#include <iostream>
+
+int main()
+{
+  
+}
+
+
